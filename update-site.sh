b7f2d36321b1c258e112dfa3603d0714444bcec5
Marion Le Borgne <leborgne.mar@gmail.com>
2015-08-07 21:42:52 -0700

    Update update-site.sh

--- update-site.sh
+++ update-site.sh
@@ -2,4 +2,5 @@ git pull
 rm -rf /var/www/neurotechx.com.backup
 cp -r /var/www/neurotechx.com /var/www/neurotechx.com.backup
 rm -rf /var/www/neurotechx.com/public_html /var/www/neurotechx.com/sf
-cp -r * /var/www/neurotechx.com/
+cp -r sf /var/www/neurotechx.com/
+cp -r public_html /var/www/neurotechx.com/
