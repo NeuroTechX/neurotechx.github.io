b50c00395d0d2af0561232a423e12333e37bcae0
John Mazzucco <John.Mazzucco@gmail.com>
2016-02-19 13:28:04 -0500

    change path to the TwitterAPIExchange wrapper and remove hard-coded API keys

--- mtl/twitter-count.php
+++ mtl/twitter-count.php
@@ -1,12 +1,12 @@
 <?php
-    require_once('TwitterAPIExchange.php'); //get it from https://github.com/J7mbo/twitter-api-php
+    require_once('../TwitterAPIExchange.php'); //get it from https://github.com/J7mbo/twitter-api-php
 
     /** Set access tokens here - see: https://dev.twitter.com/apps/ **/
     $settings = array(
-        'oauth_access_token' => "2411112222-Uu1lAaJUIf5QdGvipgRXX3XkVSWjUdiNpnfBswZ",
-        'oauth_access_token_secret' => "dQlWnGVgOgcfd8z02lUfAveyOvkIQHpvTyeaF1ZpsPGve",
-        'consumer_key' => "ZXfx4IgnYLp9aMfCEQGIPzsNQ",
-        'consumer_secret' => "ztmjJWI1owuYmdhBfCKdkyejxGW5MfCDKuFOOXfRvmAy6pIJ3Y"
+        'oauth_access_token' => " ",
+        'oauth_access_token_secret' => " ",
+        'consumer_key' => " ",
+        'consumer_secret' => " "
     );
 
     $ta_url = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
