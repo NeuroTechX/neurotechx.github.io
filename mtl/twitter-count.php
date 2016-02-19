b9a8ee8381f8aa8ba10f3a530441892451c12da5
John Mazzucco <John.Mazzucco@gmail.com>
2016-02-19 13:35:53 -0500

    fix path to API wrapper, change Twitter endpoint URL and display a count of the followers' IDs

--- mtl/twitter-count.php
+++ mtl/twitter-count.php
@@ -1,5 +1,5 @@
 <?php
-    require_once('../TwitterAPIExchange.php'); //get it from https://github.com/J7mbo/twitter-api-php
+    require_once('../libs/TwitterAPIExchange.php'); //get it from https://github.com/J7mbo/twitter-api-php
 
     /** Set access tokens here - see: https://dev.twitter.com/apps/ **/
     $settings = array(
@@ -9,14 +9,15 @@
         'consumer_secret' => " "
     );
 
-    $ta_url = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
+    $url = 'https://api.twitter.com/1.1/followers/ids.json';
     $getfield = '?screen_name=NeurotechMTL';
     $requestMethod = 'GET';
     $twitter = new TwitterAPIExchange($settings);
-    $follow_count=$twitter->setGetfield($getfield)
-    ->buildOauth($ta_url, $requestMethod)
-    ->performRequest();
+    $follow_count = $twitter->setGetfield($getfield)
+                 ->buildOauth($url, $requestMethod)
+                 ->performRequest();
+
     $data = json_decode($follow_count, true);
-    $followers_count=$data[0]['user']['followers_count'];
+    $followers_count= count($data['ids']);
     echo $followers_count;
 ?>
