65030bc58a45a8ec76bf9b57fe22f1116ade3f3c
John Mazzucco <John.Mazzucco@gmail.com>
2016-02-19 16:35:17 -0500

    set Twitter API keys and tokens using variables rather than hard-coding them in

--- mtl/twitter-count.php
+++ mtl/twitter-count.php
@@ -1,12 +1,15 @@
 <?php
     require_once('../libs/TwitterAPIExchange.php'); //get it from https://github.com/J7mbo/twitter-api-php
+    require_once('../apikeys.php');
+
+    $keys_and_tokens = $mtl_keys_and_tokens["twitter"];
 
     /** Set access tokens here - see: https://dev.twitter.com/apps/ **/
     $settings = array(
-        'oauth_access_token' => " ",
-        'oauth_access_token_secret' => " ",
-        'consumer_key' => " ",
-        'consumer_secret' => " "
+        'oauth_access_token' => $keys_and_tokens["token"],
+        'oauth_access_token_secret' => $keys_and_tokens["token_secret"],
+        'consumer_key' => $keys_and_tokens["key"],
+        'consumer_secret' => $keys_and_tokens["key_secret"]
     );
 
     $url = 'https://api.twitter.com/1.1/followers/ids.json';
@@ -16,7 +19,6 @@
     $follow_count = $twitter->setGetfield($getfield)
                  ->buildOauth($url, $requestMethod)
                  ->performRequest();
-
     $data = json_decode($follow_count, true);
     $followers_count= count($data['ids']);
     echo $followers_count;
