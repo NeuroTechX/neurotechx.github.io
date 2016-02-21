f1e1492d6c88e7775fc505364c3215a180dbad5e
John Mazzucco <John.Mazzucco@gmail.com>
2016-02-20 21:41:16 -0500

    echo the total number of members for the NeuroTechMTL meetup group

--- mtl/meetup-count.php
+++ mtl/meetup-count.php
@@ -1,4 +1,15 @@
 <?php
 	require_once('../libs/MeetupAPI.php');
 
+	$meetup = new Meetup(array(
+	  'key' => ""
+	));
+
+	$response = $meetup->getGroups(array(
+    'group_urlname' => 'NeuroTechMTL'
+	));
+
+	$members_count = $response->results[0]->members;
+
+	echo $members_count;
 ?>
