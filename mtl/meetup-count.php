2f79b1952b2df6b9f4bb2d747a64ab7ea4361e50
John Mazzucco <John.Mazzucco@gmail.com>
2016-02-20 21:56:51 -0500

    change indentation from tabs to spaces

--- mtl/meetup-count.php
+++ mtl/meetup-count.php
@@ -1,16 +1,16 @@
 <?php
-	require_once('../libs/MeetupAPI.php');
-	require_once('../apikeys.php');
+  require_once('../libs/MeetupAPI.php');
+  require_once('../apikeys.php');
 
-	$meetup = new Meetup(array(
-	  'key' => $mtl_keys_and_tokens["meetup"]['key']
-	));
+  $meetup = new Meetup(array(
+    'key' => $mtl_keys_and_tokens["meetup"]['key']
+  ));
 
-	$response = $meetup->getGroups(array(
+  $response = $meetup->getGroups(array(
     'group_urlname' => 'NeuroTechMTL'
-	));
+  ));
 
-	$members_count = $response->results[0]->members;
+  $members_count = $response->results[0]->members;
 
-	echo $members_count;
+  echo $members_count;
 ?>
--- mtl/twitter-count.php
+++ mtl/twitter-count.php
@@ -1,25 +1,25 @@
 <?php
-    require_once('../libs/TwitterAPIExchange.php'); //get it from https://github.com/J7mbo/twitter-api-php
-    require_once('../apikeys.php');
+  require_once('../libs/TwitterAPIExchange.php'); //get it from https://github.com/J7mbo/twitter-api-php
+  require_once('../apikeys.php');
 
-    $keys_and_tokens = $mtl_keys_and_tokens["twitter"];
+  $keys_and_tokens = $mtl_keys_and_tokens["twitter"];
 
-    /** Set access tokens here - see: https://dev.twitter.com/apps/ **/
-    $settings = array(
-        'oauth_access_token' => $keys_and_tokens["token"],
-        'oauth_access_token_secret' => $keys_and_tokens["token_secret"],
-        'consumer_key' => $keys_and_tokens["key"],
-        'consumer_secret' => $keys_and_tokens["key_secret"]
-    );
+  /** Set access tokens here - see: https://dev.twitter.com/apps/ **/
+  $settings = array(
+      'oauth_access_token' => $keys_and_tokens["token"],
+      'oauth_access_token_secret' => $keys_and_tokens["token_secret"],
+      'consumer_key' => $keys_and_tokens["key"],
+      'consumer_secret' => $keys_and_tokens["key_secret"]
+  );
 
-    $url = 'https://api.twitter.com/1.1/followers/ids.json';
-    $getfield = '?screen_name=NeurotechMTL';
-    $requestMethod = 'GET';
-    $twitter = new TwitterAPIExchange($settings);
-    $follow_count = $twitter->setGetfield($getfield)
-                 ->buildOauth($url, $requestMethod)
-                 ->performRequest();
-    $data = json_decode($follow_count, true);
-    $followers_count= count($data['ids']);
-    echo $followers_count;
+  $url = 'https://api.twitter.com/1.1/followers/ids.json';
+  $getfield = '?screen_name=NeurotechMTL';
+  $requestMethod = 'GET';
+  $twitter = new TwitterAPIExchange($settings);
+  $follow_count = $twitter->setGetfield($getfield)
+               ->buildOauth($url, $requestMethod)
+               ->performRequest();
+  $data = json_decode($follow_count, true);
+  $followers_count= count($data['ids']);
+  echo $followers_count;
 ?>
