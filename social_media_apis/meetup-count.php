42137c71595f63f7a9fd60bc14831af2c9fafc93
John Mazzucco <John.Mazzucco@gmail.com>
2016-02-26 14:59:00 -0500

    remove unused code for hiding errors

--- social_media_apis/meetup-count.php
+++ social_media_apis/meetup-count.php
@@ -1,7 +1,6 @@
 <?php
 
   error_reporting(0);
-  // ini_set('display_errors', 0);
 
   include(dirname(__FILE__, 2) . "/libs/MeetupAPI.php");
   include(dirname(__FILE__) . "/apikeys.php");
@@ -28,8 +27,6 @@
     ${($city) . '_meetup_count'} = $membersCount;
 
     $totalCount += $membersCount;
-
-
 
   };
 
