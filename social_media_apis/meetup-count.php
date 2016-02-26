0a27f4a08e77e2c843c3e1c5ab641a5a99bdd85d
John Mazzucco <John.Mazzucco@gmail.com>
2016-02-26 10:13:24 -0500

    If an error is returned when requesting the members count, assign an empty string to a variable rather than the returned value

--- social_media_apis/meetup-count.php
+++ social_media_apis/meetup-count.php
@@ -16,11 +16,22 @@
       'group_urlname' => "NeuroTech{$city}"
     ));
 
-    // Assign the members count of a city's meetup group to a separate variable.
-    // For example, the member count of Montreal's meetup group will be $MTL_meetup_count
-    ${$city . '_meetup_count'} = $response->results[0]->members;
+    $membersCount = $response->results[0]->members;
 
-    $totalCount += $response->results[0]->members;
+
+    if (is_int($membersCount)){
+
+      // Assign the members count of a city's meetup group to a unique variable.
+      // ex. the member count of Montreal's meetup group will be $MTL_meetup_count
+      ${$city . '_meetup_count'} = $membersCount
+      $totalCount += $membersCount
+
+    } else {
+
+      // Assign the variable to any empty string if $membersCount is not an integer. This will prevent an error message from displaying on the site
+      ${$city . '_meetup_count'} = ""
+
+    };
 
   }
 
