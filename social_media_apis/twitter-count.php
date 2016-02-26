a048c4872141e5eca858b583c02a1a6752e22577
John Mazzucco <John.Mazzucco@gmail.com>
2016-02-26 15:52:20 -0500

    loop through the array of twitter handle suffixes and get the followers count for each twitter account

--- social_media_apis/twitter-count.php
+++ social_media_apis/twitter-count.php
@@ -1,27 +1,34 @@
 <?php
+
   include(dirname(__FILE__, 2) . "/libs/TwitterAPIExchange.php"); //get it from https://github.com/J7mbo/twitter-api-php
   include(dirname(__FILE__) . "/apikeys.php");
 
-  $twitterHandleSuffix = array('x', 'mtl', 'sf', 'bos', 'to', 'nyc', 'xnews');
-
-  $keys_and_tokens = $mtl_keys_and_tokens["twitter"];
+  $twitterHandleSuffixes = array('x', 'mtl', 'sf', 'bos', 'to', 'nyc', 'xnews');
 
   /** Set access tokens here - see: https://dev.twitter.com/apps/ **/
   $settings = array(
-      'oauth_access_token' => $keys_and_tokens["token"],
-      'oauth_access_token_secret' => $keys_and_tokens["token_secret"],
-      'consumer_key' => $keys_and_tokens["key"],
-      'consumer_secret' => $keys_and_tokens["key_secret"]
+      'oauth_access_token' => $twitter_keys_and_tokens["token"],
+      'oauth_access_token_secret' => $twitter_keys_and_tokens["token_secret"],
+      'consumer_key' => $twitter_keys_and_tokens["key"],
+      'consumer_secret' => $twitter_keys_and_tokens["key_secret"]
   );
 
   $url = 'https://api.twitter.com/1.1/followers/ids.json';
-  $getfield = '?screen_name=NeurotechMTL';
   $requestMethod = 'GET';
-  $twitter = new TwitterAPIExchange($settings);
-  $follow_count = $twitter->setGetfield($getfield)
-               ->buildOauth($url, $requestMethod)
-               ->performRequest();
-  $data = json_decode($follow_count, true);
-  $followers_count= count($data['ids']);
-  echo $followers_count;
+
+
+  foreach ($twitterHandleSuffixes as $suffix) {
+
+    $getfield = '?screen_name=Neurotech' . $suffix;
+
+    $twitter = new TwitterAPIExchange($settings);
+    $follow_count = $twitter->setGetfield($getfield)
+                 ->buildOauth($url, $requestMethod)
+                 ->performRequest();
+    $data = json_decode($follow_count, true);
+
+    ${'neurotech' . ($suffix) . '_followers_count'} = count($data['ids']);
+
+  };
+
 ?>
