ba58e29a570de68ca9934b87597596835a47c9bd
alfalfasalads <ana.taveramendoza@gmail.com>
2016-02-26 19:56:18 -0500

    First iteration [wip]
    First iteration for making Rouffi's wireframe a reality.
    Still in progress, needs content.
    
    I'll begin setting up the testing environment if needed.

--- ntX_web_phase1/js/template.js
+++ ntX_web_phase1/js/template.js
@@ -9,12 +9,23 @@
 /*jshint strict: true */
 
 
-// jQuery to collapse the navbar on scroll
+
+//jQuery to hide menu on load
+$(document).ready(function () {
+    "use strict";
+	// hide .navbar first
+	$(".navbar").hide();
+	
+});
+
+// jQuery to unhide + collapse the navbar on scroll
 $(window).scroll(function () {
 	"use strict";
     if ($(".navbar").offset().top > 50) {
+		$(".navbar").fadeIn();
         $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
+		$(".navbar").fadeOut();
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
     }
 });
