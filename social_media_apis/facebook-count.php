84f5058f659b7b43f10fe9a64153b50abf173ce6
John Mazzucco <John.Mazzucco@gmail.com>
2016-02-29 13:54:17 -0500

    Add group members count to an array and output it as a JSON file

--- social_media_apis/facebook-count.php
+++ social_media_apis/facebook-count.php
@@ -2,6 +2,8 @@
 
 include(dirname(__FILE__, 2) . "/vendor/autoload.php");
 
+$members_count_array = array('facebookMembersCount' => array());
+
 $fb = new Facebook\Facebook([
   'app_id' => '',
   'app_secret' => '',
@@ -12,7 +14,11 @@ $fb = new Facebook\Facebook([
 // The Facebook Group ID of NeuroTechX is 1607814756147186
 $response = $fb->get('/1607814756147186/members?limit=500');
 
-$object = $response->getGraphEdge();
-$members_count = count($object);
+// Manually adding 14 to the count as 14 group members are consistently missing from the API response
+$members_count = count($response->getGraphEdge()) + 14;
+$members_count_array['facebookMembersCount']['neurotechx'] = $members_count;
+
+echo json_encode($members_count_array);
+
 
 ?>
