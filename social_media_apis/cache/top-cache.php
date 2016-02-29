0befe28adad7261e15507c8a94d8a744abf84360
John Mazzucco <John.Mazzucco@gmail.com>
2016-02-29 09:01:37 -0500

    Change expiry time of cache files from 30 seconds to 30 minutes

--- social_media_apis/cache/top-cache.php
+++ social_media_apis/cache/top-cache.php
@@ -6,7 +6,7 @@ $break = Explode('/', $url);
 $file = $break[count($break) - 1];
 
 $cachefile = dirname(__FILE__).'/cached-'.substr_replace($file ,"",-4).'.JSON';
-$cachetime = 30;
+$cachetime = 1800; // 30 minutes
 
 // Serve from the cache if it is younger than $cachetime
 if (file_exists($cachefile) && time() - $cachetime < filemtime($cachefile)) {
