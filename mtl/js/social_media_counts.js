6ed75ba5b778ecda977329caffd64875b202f5f7
John Mazzucco <John.Mazzucco@gmail.com>
2016-02-29 14:24:31 -0500

    Add Facebook members count for future use

--- mtl/js/social_media_counts.js
+++ mtl/js/social_media_counts.js
@@ -6,3 +6,7 @@ $.getJSON( "../social_media_apis/twitter-count.php", function( countsArray ) {
 	$(".twitter-count").text(countsArray.twitterFollowersCount.neurotechMtl);
 });
 
+// Add a "facebook-count" class in index.html to display the members count of the NeuroTechX facebook group
+$.getJSON( "../social_media_apis/facebook-count.php", function( countsArray ) {
+	$(".facebook-count").text(countsArray.facebookMembersCount.neurotechx);
+});
