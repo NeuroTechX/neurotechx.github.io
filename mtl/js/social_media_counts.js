1c009a8e6d6fdde5940072f8025732057ca50ddf
John Mazzucco <John.Mazzucco@gmail.com>
2016-02-29 09:09:25 -0500

    Get Twitter followers count and insert it in Index.html

--- mtl/js/social_media_counts.js
+++ mtl/js/social_media_counts.js
@@ -1,4 +1,8 @@
 $.getJSON( "../social_media_apis/meetup-count.php", function( countsArray ) {
-	$(".meetup").text(countsArray.meetupMembersCount.mtl);
+	$(".meetup-count").text(countsArray.meetupMembersCount.mtl);
+});
+
+$.getJSON( "../social_media_apis/twitter-count.php", function( countsArray ) {
+	$(".twitter-count").text(countsArray.twitterFollowersCount.mtl);
 });
 
