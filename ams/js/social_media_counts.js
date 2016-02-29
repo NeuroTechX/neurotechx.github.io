1c74ba6cff9436d1afeb98aee2a9e2a8bbc27565
John Mazzucco <John.Mazzucco@gmail.com>
2016-02-29 15:00:50 -0500

    Setup twitter count for future use

--- ams/js/social_media_counts.js
+++ ams/js/social_media_counts.js
@@ -4,6 +4,12 @@ $.getJSON( "../social_media_apis/meetup-count.php", function( countsArray ) {
 	$(".meetup-count").text(countsArray.meetupMembersCount.ams);
 });
 
+// Twitter account for NeuroTechAMS currently does not exist
+
+// $.getJSON( "../social_media_apis/twitter-count.php", function( countsArray ) {
+// 	$(".twitter-count").text(countsArray.twitterFollowersCount.neurotechAms);
+// });
+
 $.getJSON( "../social_media_apis/facebook-count.php", function( countsArray ) {
 	$(".facebook-count").text(countsArray.facebookMembersCount.neurotechx);
 });
