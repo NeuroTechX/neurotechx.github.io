a9762dd811d626152681a5b60b7f612634ed03ec
John Mazzucco <John.Mazzucco@gmail.com>
2016-02-29 14:12:18 -0500

    Convert tabs to spaces

--- social_media_apis/apiKeys.php
+++ social_media_apis/apiKeys.php
@@ -1,19 +1,17 @@
 <?php
 
-	$meetup_key = "";
+  $meetup_key = "";
 
-	$twitter_keys_and_tokens = array(
-		"key"=>"",
-		"key_secret"=>"",
-		"token"=>"",
-		"token_secret"=>""
-	);
+  $twitter_keys_and_tokens = array(
+    "key"=>"",
+    "key_secret"=>"",
+    "token"=>"",
+    "token_secret"=>""
+  );
 
-	$facebook_keys_and_tokens = array(
-		"app_id"=>"",
-  	"app_secret"=>""
-	);
+  $facebook_keys_and_tokens = array(
+    "app_id"=>"",
+    "app_secret"=>""
+  );
 
 ?>
-
-
