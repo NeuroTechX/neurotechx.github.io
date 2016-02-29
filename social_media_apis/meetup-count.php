8649fa621c2e53c3763ff2e3f5b4db9eda1ba53d
John Mazzucco <John.Mazzucco@gmail.com>
2016-02-29 14:29:48 -0500

    Remove unused code for hiding errors

--- social_media_apis/meetup-count.php
+++ social_media_apis/meetup-count.php
@@ -1,6 +1,5 @@
 <?php
 
-  // error_reporting(0); //hide all errors
   include dirname(__FILE__) . '/cache/top-cache.php';
   include(dirname(__FILE__, 2) . "/libs/MeetupAPI.php");
   include(dirname(__FILE__) . "/apikeys.php");
