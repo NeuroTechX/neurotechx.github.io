1ccb1da8d97c72c9595b858e874862316a8c398c
John Mazzucco <John.Mazzucco@gmail.com>
2016-02-29 14:07:16 -0500

    Store Facebook API keys and tokens in the apiKeys file

--- social_media_apis/apiKeys.php
+++ social_media_apis/apiKeys.php
@@ -3,10 +3,17 @@
 	$meetup_key = "";
 
 	$twitter_keys_and_tokens = array(
-		"key"=>" ",
-		"key_secret"=>" ",
-		"token"=>" ",
-		"token_secret"=>" "
+		"key"=>"",
+		"key_secret"=>"",
+		"token"=>"",
+		"token_secret"=>""
+	);
+
+	$facebook_keys_and_tokens = array(
+		"app_id"=>"",
+  	"app_secret"=>""
 	);
 
 ?>
+
+
--- social_media_apis/facebook-count.php
+++ social_media_apis/facebook-count.php
@@ -1,14 +1,18 @@
 <?php
 
 include(dirname(__FILE__, 2) . "/vendor/autoload.php");
+include(dirname(__FILE__) . "/apikeys.php");
 
 $members_count_array = array('facebookMembersCount' => array());
 
+$app_id =	$facebook_keys_and_tokens['app_id'];
+$app_secret = $facebook_keys_and_tokens['app_secret'];
+
 $fb = new Facebook\Facebook([
-  'app_id' => '',
-  'app_secret' => '',
+  'app_id' => $app_id,
+  'app_secret' => $app_secret,
   'default_graph_version' => 'v2.5',
-  'default_access_token' => ''
+  'default_access_token' => $app_id . "|" . $app_secret
   ]);
 
 // The Facebook Group ID of NeuroTechX is 1607814756147186
@@ -19,6 +23,5 @@ $members_count = count($response->getGraphEdge()) + 14;
 $members_count_array['facebookMembersCount']['neurotechx'] = $members_count;
 
 echo json_encode($members_count_array);
-
 
 ?>
