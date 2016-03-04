6c1bcc725b8e74f0f496cf1c0d0f78c790a82027
John Mazzucco <John.Mazzucco@gmail.com>
2016-03-04 16:34:28 -0500

    Add another event listener to re-center the map in the div when the browser is resized

--- ntx-web-phase1/js/map.js
+++ ntx-web-phase1/js/map.js
@@ -1,6 +1,7 @@
 // Google Maps Scripts
 // When the window has finished loading create our google map below
 google.maps.event.addDomListener(window, 'load', init);
+google.maps.event.addDomListener(window, 'resize', init);
 
 function init() {
     // Basic options for a simple Google Map
