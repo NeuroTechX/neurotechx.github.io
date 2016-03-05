7cc7c00f020bc73d8f02cbcac0814539443a3ad5
John Mazzucco <John.Mazzucco@gmail.com>
2016-03-05 16:13:46 -0500

    Get Slack channel members and output the total in a JSON object

---
+++ social_media_counts/slack-count.php
@@ -0,0 +1,26 @@
+<?php
+
+include(dirname(__FILE__) . "/apikeys.php");
+
+$url = 'https://slack.com/api/users.list?token=' . $slack_token;
+
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+$curl_response = curl_exec($curl);
+if ($curl_response === false) {
+    $info = curl_getinfo($curl);
+    curl_close($curl);
+    die('error occured during curl exec. Additioanl info: ' . var_export($info));
+}
+curl_close($curl);
+$decoded = json_decode($curl_response);
+if (isset($decoded->response->status) && $decoded->response->status == 'ERROR') {
+    die('error occured: ' . $decoded->response->errormessage);
+}
+
+$members_count_array = array('slackMembersCount' => array());
+$members_count_array['slackMembersCount'] = count($decoded->members);
+
+echo json_encode($members_count_array);
+
+?>
