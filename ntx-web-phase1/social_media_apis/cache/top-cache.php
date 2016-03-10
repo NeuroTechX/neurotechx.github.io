e5ba483072f57993c58a4343e68ab10c05f699ce
John Mazzucco <John.Mazzucco@gmail.com>
2016-03-10 17:03:10 -0500

    Increase the expiry time of cache files to 3 hours

--- ntx-web-phase1/social_media_apis/cache/top-cache.php
+++ ntx-web-phase1/social_media_apis/cache/top-cache.php
@@ -8,7 +8,7 @@ $file = $break[count($break) - 1];
 $cachefile = dirname(__FILE__).'/cached-'.substr_replace($file ,"",-4).'.JSON';
 
 //Twitter API Request limits: 15 calls every 15 minutes
-$cachetime = 20;
+$cachetime = 10800; // 3 hours
 
 // Serve from the cache if it is younger than $cachetime
 if (file_exists($cachefile) && time() - $cachetime < filemtime($cachefile)) {
