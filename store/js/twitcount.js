dad7febf5534b4e782402eccf4aa3d2f0ad955f4
FerEph <ferdinandephrem@yahoo.com>
2016-04-21 16:38:01 -0400

    Did a store page
    Created in a folder outside of public the address should be
    store.neurotechx.com

---
+++ store/js/twitcount.js
@@ -0,0 +1,16 @@
+//twitter counter api thing
+
+/*jslint browser: true*/
+/*global $, jQuery, alert, google, init*/
+/*jshint strict: true */
+
+
+var apikey = "2d8e7dadf68012ee2c204ca4d1b4e12b";
+var twitter_id = "2802579095";
+var output = JSON;
+var 
+var 
+
+var twitReq = new XMLHttpRequest();
+twitReq open("GET", "http://api.twittercounter.com/",false);
+
